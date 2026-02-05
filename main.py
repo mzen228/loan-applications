ae8e5da0541b593fc483e2fa51c7e5e278d54e9b
mzen228 <mzen228@proton.me>
2026-02-04 23:20:09 -0700

    init

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class LoanApplication(BaseModel):
+    applicant_name: str
+    amount: float
+    annual_income: float
+
+
+app = FastAPI()
+
+loan_applications = []
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/loans/{item_id}")
+def read_loan_application(loan_application_id: int):
+    for loan_application in loan_applications:
+        if loan_application_id == loan_application.get("id"):
+            return loan_application
+    return {"error": "Loan application not found"}
+
+
+@app.put("/loans/{loan_application_id}")
+def update_loan_application(loan_application: LoanApplication):
+    new_loan_application = {
+        "id": len(loan_applications) + 1,
+        "name": loan_application.applicant_name,
+        "amount": loan_application.amount,
+        "annual_income": loan_application.annual_income,
+    }
+    loan_applications.append(new_loan_application)
+    return new_loan_application
