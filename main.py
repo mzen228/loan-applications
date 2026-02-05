6c4aff718239b243722adc8ec4c51f064791adb0
mzen228 <mzen228@proton.me>
2026-02-05 13:15:41 -0700

    feat: create loan application class that is distinct from loan application creation data
    Signed-off-by: mzen228 <mzen228@proton.me>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from storage import loan_applications
-from model import LoanApplicationCreate, LoanStatus
-from datetime import datetime
+from model import LoanApplicationCreate, LoanApplication
 
 app = FastAPI()
 
@@ -9,21 +8,17 @@ app = FastAPI()
 @app.get("/loans/{loan_application_id}")
 def read_loan_application(loan_application_id: int):
     for loan_application in loan_applications:
-        if loan_application_id == loan_application.get("id"):
+        if loan_application_id == loan_application.id:
             return loan_application
     return {"error": "Loan application not found"}
 
 
 @app.post("/loans/{loan_id}")
-def create_loan_application(loan_application: LoanApplicationCreate):
-    new_loan_application = {
-        "id": len(loan_applications),
-        "name": loan_application.applicant_name,
-        "amount": loan_application.amount,
-        "annual_income": loan_application.annual_income,
-        "term_months": loan_application.term_months,
-        "status": LoanStatus.SUBMITTED,
-        "created_at": datetime.utcnow(),
-    }
+def create_loan_application(loan_application_create: LoanApplicationCreate):
+    new_loan_application = LoanApplication(
+        applicant_name=loan_application_create.applicant_name,
+        amount=loan_application_create.amount,
+        term_months=loan_application_create.term_months,
+    )
     loan_applications.append(new_loan_application)
     return new_loan_application
--- model.py
+++ model.py
@@ -11,6 +11,20 @@ class LoanStatus(Enum):
     DENIED = "denied"
 
 
+class LoanApplication:
+    ID = 0
+
+    def __init__(self, applicant_name, amount, term_months):
+        self.id = LoanApplication.ID
+        self.status = LoanStatus.SUBMITTED
+        self.created_at = datetime.utcnow()
+        self.applicant_name = applicant_name
+        self.amount = amount
+        self.term_months = term_months
+
+        LoanApplication.ID += 1
+
+
 class LoanApplicationCreate(BaseModel):
     applicant_name: str
     amount: float = 0
